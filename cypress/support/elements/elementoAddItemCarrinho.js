a3a84cd0ebdb1343a2ebaed7af8123c886e90720
Aucelio.Barbosa <aucelio.barbosa@gmail.com>
2026-05-15 22:55:49 -0300

    mapeamento dos elemento das paginas

---
+++ cypress/support/elements/elementoAddItemCarrinho.js
@@ -0,0 +1,7 @@
+export const addItemCarrinho = {
+    VALOR_ITEM:':nth-child(4) > .product-image-wrapper h2:eq(0)',
+    NOME_ITEM:':nth-child(4) > .product-image-wrapper p:eq(0)',
+    BOTAO_ADD_CARRINHO:':nth-child(4) > .product-image-wrapper a:eq(0)',
+    MODAL_TITLE:'.modal-header .modal-title',
+    BOTAO_ACC_CART_MODAL:'.modal-body a[href="/view_cart"]'
+}
